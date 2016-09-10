95a99e2bf3ac220942a84514affd87f41058230c
anupsis <ediarto@gmail.com>
2016-09-10 20:36:26 +0700

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,14 @@
+
+
+<?php
+echo "Hello apa kabar?";
+/*echo "<select name=’tahun’>";
+for ($tahun = 1930; $tahun <= 2008; $tahun++)
+{
+echo "<option value=’".$tahun."’>".$tahun."</option>";
+}
+echo "</select>";
+if($tahun = '1931')
+echo "asdf";
+*/
+?>
